0366e0d01bd10a20e682e12598191796e78af707
vensky <vensky@outlook.com>
2015-05-20 14:45:34 +0500

    Верстка первого экрана - Приветствие

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
     sass = require('gulp-ruby-sass');
     prefix = require('gulp-autoprefixer');
-
-
+    include = require('gulp-file-include');
+    svgmin = require('gulp-svgmin');
+    svgstore = require('gulp-svgstore');
+    
 gulp.task('sass', function() {
     return sass('assets/scss/style.scss') 
     .on('error', function (err) {
@@ -12,11 +14,34 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('src/css'));
 });
 
-gulp.task('copy', function() {
-    gulp.src('assets/*.html')
+gulp.task('include', function() {
+    gulp.src(['assets/index.html'])
+        .pipe(include({
+            prefix: '@@',
+            basepath: '@file'
+    }))
     .pipe(gulp.dest('src'));
 });
 
+gulp.task('svg', function () {
+    return gulp.src('assets/img/svg/*.svg')
+        // .pipe(svgmin({
+        //     plugins: [{
+        //         removeViewBox: false
+        //     }, {
+        //         removeUselessStrokeAndFill: false
+        //     },{
+        //         removeDoctype: true
+        //     },{   
+        //         removeComments: true
+        //     }]
+        // }))
+        .pipe(svgstore({
+            inlineSvg: true
+        }))
+        .pipe(gulp.dest('assets/img/'));
+});
+
 gulp.task('watch', function() {
-    gulp.watch(['assets/scss/**/*.scss', 'assets/*.html'], ['sass', 'copy']);
+    gulp.watch(['assets/scss/**/*.scss', 'assets/*.html'], ['sass', 'include']);
 });
